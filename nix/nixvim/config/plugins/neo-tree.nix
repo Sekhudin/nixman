c794072796893dc1165e09ca0bc5752c084a6eb1
Sekhudin <sekhudinuap@gmail.com>
2025-12-24 01:00:28 +0700

    chore: neo-tree plugins

--- nix/nixvim/config/plugins/neo-tree.nix
+++ nix/nixvim/config/plugins/neo-tree.nix
@@ -2,9 +2,24 @@
 
 {
   plugins.neo-tree.enable = true;
-  plugins.neo-tree.settings.close_if_last_window = true;
-  plugins.neo-tree.settings.filesystem = {
-    follow_current_file.enabled = true;
-    follow_current_file.leave_dirs_open = true;
+  plugins.neo-tree.settings = {
+    close_if_last_window = true;
+    filesystem = {
+      follow_current_file = {
+        enabled = false;
+        leave_dirs_open = false;
+      };
+      filtered_items = {
+        hide_dotfiles = true;
+        hide_gitignored = true;
+        always_show_by_pattern = [
+          ".env*"
+        ];
+      };
+    };
+    window = {
+      width = 30;
+      auto_expand_width = false;
+    };
   };
 }
